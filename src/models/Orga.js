5bf939039da154d66e61f825f9246dd37d3e6644
Tarsmio <tarsmio@gmail.com>
2026-09-16 02:06:53 +0200

    Clean data

--- src/models/Orga.js
+++ src/models/Orga.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 
 const orgaSchema = new mongoose.Schema({
-    orgaName: {type: String}
+    orgaName: { type: String }
 }, { timestamps: true });
+
+const cleanOrgaData = async (dataIds, mongooseContext) => {
+    const ids = Array.isArray(dataIds) ? dataIds : [dataIds];
+    if (ids.length === 0) return;
+
+    await mongooseContext.model('AssoMember').deleteMany({ orga: { $in: ids } });
+};
+
+commandSchema.pre('deleteOne', { document: true, query: false }, async function () {
+    await cleanOrgaData(this._id, this);
+});
+
+commandSchema.pre(['findOneAndDelete', 'findOneAndRemove'], async function () {
+    const doc = await this.model.findOne(this.getQuery());
+    if (doc) await cleanOrgaData(doc._id, this);
+});
+
+commandSchema.pre('deleteMany', async function () {
+    const docs = await this.model.find(this.getQuery());
+    await cleanOrgaData(docs.map(d => d._id), this);
+});
 
 module.exports = mongoose.model('Orga', orgaSchema, 'orga');
--- src/models/User.js
+++ src/models/User.js
@@ -52,6 +52,27 @@ userSchema.pre(['findOneAndUpdate', 'updateOne', 'updateMany'], async function (
   update.$inc.sessionVersion = 1;
 });
 
+const cleanUserData = async (dataIds, mongooseContext) => {
+  const ids = Array.isArray(dataIds) ? dataIds : [dataIds];
+  if (ids.length === 0) return;
+
+  await mongooseContext.model('AssoMember').deleteMany({ user: { $in: ids } });
+};
+
+commandSchema.pre('deleteOne', { document: true, query: false }, async function () {
+  await cleanUserData(this._id, this);
+});
+
+commandSchema.pre(['findOneAndDelete', 'findOneAndRemove'], async function () {
+  const doc = await this.model.findOne(this.getQuery());
+  if (doc) await cleanUserData(doc._id, this);
+});
+
+commandSchema.pre('deleteMany', async function () {
+  const docs = await this.model.find(this.getQuery());
+  await cleanUserData(docs.map(d => d._id), this);
+});
+
 userSchema.methods.correctPassword = async function (givenPwd, dbPwd) {
   return await bcrypt.compare(givenPwd, dbPwd)
 }
